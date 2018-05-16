fe9869022093ebfc58247f321a660e2a2288212b
Simon Benjamin Hunsberger <simohuns@gmail.com>
2018-05-16 14:42:52 -0400

    Move page menu to global variables and CSS menu fixes

---
+++ NewParticleArray/GlobalVariables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace NewParticleArray
+{
+    public static class GlobalVariable
+    {
+        public static readonly IList<string> Pages = new ReadOnlyCollection<string>(new List<string> { "Home", "About", "Projects", "Contact" });
+    }
+}
